d680948fd1329cf2abb0c5b910ec07d935fa3248
Atin Gupta <atingupta@email.com>
2024-12-11 09:13:27 +0530

    -

--- day-3/mini-project/hands-on/bookmarks/commands.sh
+++ day-3/mini-project/hands-on/bookmarks/commands.sh
@@ -11,3 +11,20 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
 python manage.py runserver 0.0.0.0:8000
+
+
+## For Docker:
+docker compose up -d
+
+# Optional commands
+docker compose logs
+docker compose logs
+docker compose logs -f
+#docker compose exec web_run bash
+docker compose ps
+docker compose exec web_run python /code/bookmarks/manage.py migrate
+docker compose exec web_run python /code/bookmarks/manage.py createsuperuser
+docker compose stop
+docker compose restart
+docker compose down
+docker images
