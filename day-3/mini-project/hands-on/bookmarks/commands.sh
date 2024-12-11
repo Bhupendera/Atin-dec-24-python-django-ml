2b3ff961d8ecab096e4e037314f6c6442f7bab53
Atin Gupta <atingupta@email.com>
2024-12-11 13:05:00 +0530

    -

--- day-3/mini-project/hands-on/bookmarks/commands.sh
+++ day-3/mini-project/hands-on/bookmarks/commands.sh
@@ -1,13 +1,16 @@
 cd day-3\mini-project\hands-on\bookmarks
 python -m venv venv
 venv\Scripts\activate.bat
-set PIP_DISABLE_PIP_VERSION_CHECK=1
-set PYTHONDONTWRITEBYTECODE=1
-set PYTHONUNBUFFERED=1
+#set PIP_DISABLE_PIP_VERSION_CHECK=1
+#set PYTHONDONTWRITEBYTECODE=1
+#set PYTHONUNBUFFERED=1
 pip install -r requirements.txt
 pip install psycopg[binary]
 
 python manage.py makemigrations
+python manage.py migrate
+python manage.py makemigrations account
+python manage.py makemigrations images
 python manage.py migrate
 python manage.py createsuperuser
 python manage.py runserver 0.0.0.0:8000
